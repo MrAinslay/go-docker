9ca06af51fd569cf1865882ce280a30ec628a381
MrAinslay <asteriospaligiannis@gmail.com>
2024-01-28 23:09:29 +0200

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
@@ -19,6 +20,9 @@ func main() {
 
 func parent() {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -37,5 +41,12 @@ func child() {
 
 	if err := cmd.Run(); err != nil {
 		log.Println("ERROR", err)
+		os.Exit(1)
+	}
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
