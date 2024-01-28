3691f85bb14201408bcfacd1b8a40dfa5fe649c7
MrAinslay <asteriospaligiannis@gmail.com>
2024-01-28 23:03:58 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,2 +1,41 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		parent()
+	case "child":
+		child()
+	default:
+		panic("what should I do")
+	}
+}
+
+func parent() {
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Println("ERROR", err)
+		os.Exit(1)
+	}
+}
+
+func child() {
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Println("ERROR", err)
+	}
+}
